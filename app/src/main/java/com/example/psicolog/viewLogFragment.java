12e7f6c81d2334f1e881d82d930e9a405ae3e78b
david <david@elpuig.xeill.net>
2025-03-19 10:48:47 +0100

    Borrar log. Pantalla de visualización terminada Logo de PsicoLog

--- app/src/main/java/com/example/psicolog/viewLogFragment.java
+++ app/src/main/java/com/example/psicolog/viewLogFragment.java
@@ -1,5 +1,6 @@
 package com.example.psicolog;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,14 +9,24 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
 import java.util.Map;
+
+import io.appwrite.Client;
+import io.appwrite.coroutines.CoroutineCallback;
+import io.appwrite.services.Databases;
 
 
 public class viewLogFragment extends Fragment {
@@ -25,8 +36,12 @@ public class viewLogFragment extends Fragment {
     TextView sleepTime;
     TextView sex;
     TextView logContent;
+    Button delete;
 
     AppViewModel viewModel;
+    String logId;
+
+    Client client;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,11 +54,15 @@ public class viewLogFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        client = new Client(requireContext())
+                .setProject(getString(R.string.APPWRITE_PROJECT_ID)); // Your project ID
+
         date = view.findViewById(R.id.dateTextView);
         wellness = view.findViewById(R.id.wellnessTextView);
         sleepTime = view.findViewById(R.id.sleepTimeTextView);
         sex = view.findViewById(R.id.sexTextView);
         logContent = view.findViewById(R.id.logContentTextView);
+        delete = view.findViewById(R.id.deleteButton);
 
         viewModel = new ViewModelProvider(getActivity()).get(AppViewModel.class);
 
@@ -51,18 +70,66 @@ public class viewLogFragment extends Fragment {
             @Override
             public void onChanged(Map<String, Object> stringObjectMap) {
                 String dateStr = stringObjectMap.get("$id").toString();
+                logId = dateStr;
 
                 Calendar day = Calendar.getInstance();
                 day.set(Integer.parseInt(dateStr.substring(0,4)), Integer.parseInt(dateStr.substring(4,6)), Integer.parseInt(dateStr.substring(6,8)));
 
                 String[] dias_semana={ "Jueves", "Viernes", "Sábado", "Domingo", "Lunes", "Martes", "Miércoles" };
-                date.setText(dias_semana[day.get(Calendar.DAY_OF_WEEK)-1]+", "+day.get(Calendar.DAY_OF_MONTH)+" de "+day.get(Calendar.MONTH)+" de "+day.get(Calendar.YEAR));
+                String[] meses={ "Diciembre", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre"};
+                date.setText(dias_semana[day.get(Calendar.DAY_OF_WEEK)-1]+", "+day.get(Calendar.DAY_OF_MONTH)+" de "+meses[day.get(Calendar.MONTH)]+" de "+day.get(Calendar.YEAR));
                 wellness.setText("Nivel de bienestar: "+stringObjectMap.get("wellness").toString());
                 int []colors = {R.color.wellness1, R.color.wellness2, R.color.wellness3, R.color.wellness4, R.color.wellness5};
                 wellness.setBackgroundColor(ContextCompat.getColor(getContext(), colors[Integer.parseInt(stringObjectMap.get("wellness").toString()) - 1]));
-                sleepTime.setText("Horas de sueño: "+stringObjectMap.get("sleep_time").toString());
-                sex.setText("Actividad sexual: "+stringObjectMap.get("sexual_activity").toString());
+                if(stringObjectMap.get("sleep_time") != null)
+                    sleepTime.setText("Horas de sueño: "+stringObjectMap.get("sleep_time").toString());
+                else
+                    sleepTime.setText("Horas de sueño: ?");
+                if(stringObjectMap.get("sexual_Activity") != null)
+                    sex.setText("Actividad sexual: "+stringObjectMap.get("sexual_activity").toString());
+                else
+                    sex.setText("Actividad sexual: ?");
                 logContent.setText(stringObjectMap.get("comments").toString());
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Confirmación")
+                        .setMessage("¿Estás seguro de borrar el log?")
+                        .setPositiveButton("Sí", (dialog, which) -> {
+                            // Acción al pulsar "Sí"
+
+                            Databases databases = new Databases(client);
+                            Handler mainHandler = new Handler(Looper.getMainLooper());
+
+                            databases.deleteDocument(
+                                    getString(R.string.APPWRITE_DATABASE_ID), // databaseId
+                                    getString(R.string.APPWRITE_DAYLYLOGS_COLLECTION_ID), // collectionId
+                                    logId,
+                                    new CoroutineCallback<>((result, error) -> {
+                                        if (error != null) {
+                                            Snackbar.make(requireView(), "Error al borrar el post: " + error.toString(), Snackbar.LENGTH_LONG).show();
+                                            return;
+                                        }
+
+                                        System.out.println( result.toString() );
+
+                                        mainHandler.post(() -> { Navigation.findNavController(view).popBackStack(); });
+
+                                    })
+                            );
+                        })
+                        .setNegativeButton("No", (dialog, which) -> {
+                            // Acción al pulsar "No"
+                            System.out.println("Se canceló la acción.");
+                        });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
     }
